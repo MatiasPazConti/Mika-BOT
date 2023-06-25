a8c16e6e13376bbe241dbe4b7fb4c3d8d883eed0
Matias Paz Conti <matias.paz.conti@fing.edu.uy>
2023-06-24 21:25:05 -0300

    V0.1.8: Added Custom Embed create and add-field functions

---
+++ src/commands/embeds/add-embed-field.js
@@ -0,0 +1,84 @@
+const {
+  PermissionFlagsBits,
+  ApplicationCommandOptionType,
+  MessageEmbed,
+} = require("discord.js");
+
+module.exports = {
+  name: "add-embed-field",
+  description: "Descripción",
+  options: [
+    {
+      name: "canal",
+      description: "Ingrese el canal con el Embed a modificar.",
+      type: ApplicationCommandOptionType.Channel,
+      required: true,
+    },
+    {
+      name: "mensaje",
+      description: "Ingrese el ID del mensaje con el Embed a modificar.",
+      type: ApplicationCommandOptionType.String,
+      required: true,
+    },
+    {
+      name: "título",
+      description: "Ingrese el título de la nueva sección.",
+      type: ApplicationCommandOptionType.String,
+      required: true,
+    },
+    {
+      name: "descripción",
+      description:
+        "Ingrese la descripción de la nueva sección.\n" +
+        "Nota: Para hacer Saltos de Línea use /n/ y para hacer Tabulaciones use /t/",
+      type: ApplicationCommandOptionType.String,
+      required: true,
+    },
+  ],
+  permissionsRequired: [PermissionFlagsBits.Administrator],
+  botPermissions: [PermissionFlagsBits.SendMessages],
+
+  callback: async (client, interaction) => {
+    if (!interaction.inGuild()) {
+      interaction.reply({
+        content: "Este comando solo puede usarse en servidores.",
+        ephemeral: true,
+      });
+      return;
+    }
+
+    try {
+      await interaction.deferReply();
+
+      const channelId = interaction.options.get("canal").value;
+      const messageId = interaction.options.get("mensaje");
+      const fieldTitle = interaction.options.get("título");
+      const fieldDescription = interaction.options.get("descripción");
+
+      let convertedDescription = "";
+      const jumpArray = fieldDescription.split("/n/");
+      for (let jump = 0; jump <= jumpArray.length; jump++) {
+        if (jump > 0) {
+          convertedDescription = convertedDescription + "\n";
+        }
+        let tabArray = jumpArray[jump].split("/t/");
+        for (let tab = 0; tab <= tabArray.length; tab++) {
+          if (tab > 0) {
+            convertedDescription = convertedDescription + "\t";
+          }
+          convertedDescription = convertedDescription + tabArray[tab];
+        }
+      }
+      const channel = client.channels.cache.get(channelId);
+      const message = await channel.messages.fetch(messageId);
+      const embed = message.embeds[0];
+      embed.addField(fieldTitle, convertedDescription);
+
+      message.edit({ embeds: [embed] });
+
+      await interaction.deleteReply();
+    } catch (error) {
+      console.log(`Hubo un error enviando el Embed:\n${error}`);
+    }
+  },
+};

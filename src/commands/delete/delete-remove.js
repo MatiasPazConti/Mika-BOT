b7c2ce19408e83f2264a2543b149c5d228a89488
Matias Paz Conti <matias.paz.conti@fing.edu.uy>
2023-07-09 20:55:49 -0300

    Removed unnecesary delete-command files

--- src/commands/delete/delete-remove.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: "welcome-channel-remove",
-  description: "Ingresar en 'name' el nombre del comando a borrar.",
-  deleted: true,
-
-  callback: async (interaction, client) => {
-    return;
-  },
-};
